d6a1dce72e432322945143cf374785cc9300015e
pursol <pursol@163.com>
2020-03-23 19:24:03 +0800

    update .gitignore

---
+++ src/main/java/A.java
@@ -0,0 +1,5 @@
+/**
+ * Created by pursol on 2020/3/23.
+ */
+public class A {
+}
---
+++ src/main/java/B.java
@@ -0,0 +1,8 @@
+/**
+ * Created by pursol on 2020/3/23.
+ */
+public class B {
+    public static void main(String[] args) {
+        System.out.println("GitVersion 0.1");
+    }
+}
